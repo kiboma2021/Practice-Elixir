51de41ba93b6bea8c249f6599f05904919b0ef6c
Ben Kiboma <omayioben@gmail.com>
2022-09-18 22:19:16 +0300

    Get string length

--- functions.ex
+++ functions.ex
@@ -1,5 +1,5 @@
 defmodule MyFunction do
-  def main do
+  def get_name do
     name = IO.gets("What is your name?") |> String.trim
     IO.puts "Hello #{name}"
   end
@@ -12,5 +12,10 @@ defmodule MyFunction do
   def data_float do
     my_float =24.25
     IO.puts "Is it a float? #{is_float(my_float)}"
+  end
+
+  def data_string do
+    my_string = "Hey Ben! Good progress in studying Elexir"
+    IO.puts "Length: #{String.length(my_string)}"
   end
 end
