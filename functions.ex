8c27a67ca5ff0494779d682c75069df6f8201382
Ben Kiboma <omayioben@gmail.com>
2022-09-18 22:00:27 +0300

    Check if it is an integer

--- functions.ex
+++ functions.ex
@@ -3,4 +3,9 @@ defmodule MyFunction do
     name = IO.gets("What is your name?") |> String.trim
     IO.puts "Hello #{name}"
   end
+
+  def data_stuff do
+    my_int = 123
+    IO.puts "Is this an Integer? #{is_integer(my_int)}"
+  end
 end
