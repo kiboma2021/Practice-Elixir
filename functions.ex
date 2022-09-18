b90ecc152aacb8dc0db9a5c4b3667aaa9538ddb8
Ben Kiboma <omayioben@gmail.com>
2022-09-18 22:06:29 +0300

    Check if float

--- functions.ex
+++ functions.ex
@@ -4,8 +4,13 @@ defmodule MyFunction do
     IO.puts "Hello #{name}"
   end
 
-  def data_stuff do
+  def data_integer do
     my_int = 123
     IO.puts "Is this an Integer? #{is_integer(my_int)}"
+  end
+
+  def data_float do
+    my_float =24.25
+    IO.puts "Is it a float? #{is_float(my_float)}"
   end
 end
